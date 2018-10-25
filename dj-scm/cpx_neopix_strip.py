ee57e5b353d9899b75efd08d3a6496a270cf6c96
Patrick Martin <patrickmartin@fastmail.com>
2018-10-24 22:05:20 -0400

    Finalized core animation. Limited to 0.08s period.

--- dj-scm/cpx_neopix_strip.py
+++ dj-scm/cpx_neopix_strip.py
@@ -1,35 +1,29 @@
 import time
 import board
 import neopixel
-from adafruit_circuitplayground.express import cpx
+from analogio import AnalogIn
+from simpleio import map_range
 import gc
 
 BLK = (0,0,0)
-rgb_array = [(50,0,0),(50,20,0),(50,50,0),
+rainbow = [(50,0,0),(50,20,0),(50,50,0),
             (0,50,0),(0,50,20),(0,50,50),
             (0,0,50),(20,0,50),(50,0,50)]
 
-#acc_x_history = []
-#acc_y_history = []
-#acc_z_history = []
-
 numpix = 30
-pix_strip_a1 = neopixel.NeoPixel(board.D6,numpix,brightness=0.3,auto_write=False)
-pix_strip_a2 = neopixel.NeoPixel(board.D9,numpix,brightness=0.3,auto_write=False)
 
-#print("free="+str(gc.mem_free()))
-def init_color_array(npix):
+pix_strip_a1 = neopixel.NeoPixel(board.D6,numpix,brightness=0.1,auto_write=False)
+pix_strip_a6 = neopixel.NeoPixel(board.D0,numpix,brightness=0.1,auto_write=False)
+
+def init_every_other(npix):
+    i = 0
     colors = [BLK for i in range(npix)]
-    color_idx = 0
-    for i in range(0,npix,2):
-        if i % len(rgb_array) == 0:
-            color_idx = 0
-        else:
-            color_idx = color_idx + 1
-        curr_color = rgb_array[color_idx]
-        colors[i] = curr_color
-        colors[i+1] = curr_color
-    return colors
+    for c in reversed(rainbow):
+        colors[i] = c
+        i += 2
+    return {'color_arr':colors, 'final_idx':i-2}
+    
+#print(init_every_other(30))
     
 def write_colors_to_strip(colors,pixels):
     i = 0
@@ -38,33 +32,34 @@ def write_colors_to_strip(colors,pixels):
         i = i+1
     pixels.show()
 
-init_colors = init_color_array(numpix)
+res = init_every_other(30)
+head_idx = res['final_idx']
+write_colors_to_strip(res['color_arr'],pix_strip_a1)
+write_colors_to_strip(res['color_arr'],pix_strip_a6)
 
-write_colors_to_strip(init_colors,pix_strip_a1)
-write_colors_to_strip(init_colors,pix_strip_a2)
-del init_colors
-prior_pix1 = pix_strip_a1[-1]
-prior_pix2 = pix_strip_a2[-1]
-idx = 0;
-idxs = range(numpix)
 is_blinking = True
+
+light_in = AnalogIn(board.LIGHT)
 while True:
-    #(acc_x,acc_y,acc_z) = cpx.acceleration
-    #print((acc_x,acc_y,acc_z))
 #    print(gc.mem_free())
     gc.collect()
-    time.sleep(0.01)
+#    light_val = light_in.value
+#    print(light_val)
+    time.sleep(0.08)
     if is_blinking:
-        for idx in idxs:
-            curr_pix1 = pix_strip_a1[idx]
-            pix_strip_a1[idx] = prior_pix1
-            prior_pix1 = curr_pix1
-            curr_pix2 = pix_strip_a2[idx]
-            pix_strip_a2[idx] = prior_pix2
-            prior_pix2 = curr_pix2
-            idx += 1
-            if idx % numpix:
-                idx = 0
+        t0 = time.monotonic()
+        
+        idx = head_idx
+        for c in rainbow:
+            pix_strip_a1[idx] = c
+            pix_strip_a6[idx] = c
+            idx -= 2
         pix_strip_a1.show()
-        pix_strip_a2.show()
+        pix_strip_a6.show()
+        t1 = time.monotonic()
+        
+        head_idx +=2
+        if head_idx % 30 == 0:
+            head_idx = 0
+        #print("Time = {0}".format(t1-t0))
     
